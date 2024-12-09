d52efa9ddac8f1dc73588a3f41bc3b25a1744523
kcan01 <73707749+kcan01@users.noreply.github.com>
2024-12-09 02:28:06 +0100

    Implement recursive tree structure with train/predict methods

--- Exercise2/regressor_tree.py
+++ Exercise2/regressor_tree.py
@@ -1,24 +1,61 @@
-import pandas as pd
 import random
+import pandas as pd
 import numpy as np
 import json
 
-random.seed(6)
+from sklearn.model_selection import train_test_split
 
-# Define column names for the dataset
-columns = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model_year', 'origin', 'car_name']
 
-data = pd.read_csv('Exercise2/auto-mpg.data', sep='\\s+', header=None, names=columns, quotechar='"')
+class Node():
 
-X_train = data.drop('mpg', axis=1)
+    def __init__(self, data, max_features=2, min_samples_split=2, max_depth=2, height=0, 
+                 left_child=None, right_child=None, flag="Internal", split_feature=None, 
+                 split_value=None, prediction=None) -> None:
+        self.data = data
+        self.max_features = max_features
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        
+        self.height = height
+        self.left_child  = left_child
+        self.right_child = right_child
+        self.flag = flag   # Internal or Leaf
+        self.split_feature = split_feature
+        self.split_value = split_value
+        self.prediction = prediction
+
+
+    def train(self):
+        # if ((len(self.data) >= self.min_samples_split) & (self.height < self.max_depth)):
+        if (self.height < self.max_depth):
+            # split procedure
+            # store results in new leftnode and rightnode
+
+            data_left, data_right = train_test_split(self.data, test_size=0.5)
+
+            self.left_child = Node(data=data_left, height=self.height + 1, max_depth=1)
+            self.right_child = Node(data=data_right, height=self.height + 1, max_depth=1)
+            self.left_child.train()
+            self.right_child.train()
+        else:
+            self.flag = "Leaf"
+            
+
+
+    def predict(self):
+        while (self.flag != "Leaf"):
+            if (self.data[self.split_feature] < self.split_value):
+                self = self.left_child
+            else:
+                self = self.right_child
+
+        return self.prediction
+
+    
 
 
 class RegressorTree():
 
-    def __init__(self, max_features) -> None:
-        self.max_features = max_features
-    
-    
     def select_random_feature(self, X_train):
         list_column_names = list(X_train.columns.values)
         feature_names = random.sample(list_column_names, self.max_features)
@@ -115,18 +152,35 @@ class RegressorTree():
 
 
 
-rtree = RegressorTree(max_features=2)
-dict_features_sorted, feature_names = rtree.select_random_feature(X_train)
-dict_average_list = rtree.calculate_average_of_two_sample_pairs(dict_features_sorted)
-optimal_split_value = rtree.get_optimal_split_value(dict_average_list, data)
-y_pred=rtree.predict(optimal_split_value,X_train)
+# rtree = RegressorTree(max_features=2)
+# dict_features_sorted, feature_names = rtree.select_random_feature(X_train)
+# dict_average_list = rtree.calculate_average_of_two_sample_pairs(dict_features_sorted)
+# optimal_split_value = rtree.get_optimal_split_value(dict_average_list, data)
+# y_pred=rtree.predict(optimal_split_value,X_train)
 
 
-print(optimal_split_value)
-print(set(y_pred))
-# print(json.dumps(dict_average_list,sort_keys=True, indent=4))
+# print(optimal_split_value)
+# print(set(y_pred))
+# # print(json.dumps(dict_average_list,sort_keys=True, indent=4))
 
-# TODO clean data input of X_train and _ytrain...
+if __name__ == "__main__":
+    random.seed(6)
+
+    # define column names for the dataset
+    columns = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model_year', 'origin', 'car_name']
+
+    # read data 
+    data = pd.read_csv('Exercise2/auto-mpg.data', sep='\\s+', header=None, names=columns, quotechar='"')
+
+    X_train = data.drop('mpg', axis=1)
+    y_train  = data["mpg"]
+
+    root = Node(data=data, max_features=2, min_samples_split=2, max_depth=1, height=0)
+    root.train()
+    print(root.right_child.flag)
+
+
+# TODO clean data input of X_train and _ytrain... --> "?" for some missing values
 
 ## Next steps
 # Marga: Implement predict function for MVP
