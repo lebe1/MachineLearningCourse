ff6567d4553f40980390ac4527a215f53dd9b3ff
marggonc <margarida.ia05@gmail.com>
2025-02-26 11:02:12 +0000

    implement random seed

--- Exercise3/agent.py
+++ Exercise3/agent.py
@@ -14,21 +14,25 @@ class Agent():
         self.exploration = exploration_rate
         #self.seed=seed
 
-    def choose_move(self, game, current_state):
-        np.random.seed(42)
+    def choose_move(self, game, current_state,seed):
+        np.random.seed(seed)
         if np.random.uniform(low=0, high=1) <= self.exploration:
             # random move
-            game.move(-1)
+            game.move(-1, seed)
+            print("RANDOM UNIFORM ")
+            print(game.board)
+            return np.zeros((3,3))
 
         else:
             # greedy move according to best value
 
             best_value = -1000000
             best_state = ""
-
+            print("CHECK ",self.board_states.get(current_state))
             # make random move if the current state hasn't been encountered yet
             if self.board_states.get(current_state) is None:
-                game.move(-1)
+                print("first move")
+                game.move(-1,seed)
                 return np.zeros((3,3))
             else:
                 for next_state in self.board_states[current_state]["next_states"]:
@@ -38,9 +42,9 @@ class Agent():
 
             matrix_best_state = []
 
-            if best_state == "":
-                print("ERROR", self.board_states[current_state])
-                print(current_state)
+            # if best_state == "":
+            #     print("ERROR", self.board_states[current_state])
+            #     print(current_state)
             best_state = best_state.split(",")
 
             for idx, letter in enumerate(best_state):
@@ -56,7 +60,7 @@ class Agent():
         #history, winner= game.play()
 
         for index, state in enumerate(reversed(game.history)):
-            print("State", state)
+            #print("State", state)
             if index==0:
                 self.board_states[state]={'value': game.winner,'next_states': set()}
                 #print("debug", self.board_states)
@@ -65,22 +69,23 @@ class Agent():
 
             if self.board_states.get(state) is None:
                 self.board_states[state] = {'value': 0,'next_states': set()}
-            print("Value beginning", self.board_states[state]['value'])
+            #print("Value beginning", self.board_states[state]['value'])
 
             self.board_states[state]['value'] = (self.board_states[state]['value'] + self.alpha * (self.board_states[old_state]['value'] - self.board_states[state]['value']))
             self.board_states[state]['next_states'].add(old_state)
 
-            print("Value afterwards", self.board_states[state]['value'])
+            #print("Value afterwards", self.board_states[state]['value'])
             old_state=state
 
         
         
-    def play(self):
+    def play(self,seed):
         game = TicTacToe()
 
-        for i in range(9):
+        for i in range(10):
             if game.EndFlag:
                 game.history.append(','.join(str(int(num)) for row in game.board for num in row))
+                print("HISTORY PLAY ", game.history)
                 break
             else:   
                 if i % 2 == 0:
@@ -90,15 +95,16 @@ class Agent():
                     print(current_state)
                     print("CURRENT BOARD: ")
                     print(game.board)
-                    new_state = self.choose_move(game, current_state)
+                    new_state = self.choose_move(game, current_state,seed)
                     if np.all(new_state == 0):
+                        print("NP.ALL")
                         continue
                     print("NEW STATE (AFTER MOVE): ")
                     print(new_state)
                     game.placement(-1, new_state)
                 else:
                     print("PLAYER RANDOM IS MOVING")
-                    game.move(1)
+                    game.move(1,seed)
 
         return game
 
@@ -106,9 +112,15 @@ class Agent():
 if __name__ == "__main__":
     agent = Agent(exploration_rate=0.2)
 
-    for i in range(10):
-        game = agent.play()
+    random.seed(63)
+    random_seed_list=random.sample(range(1,10000), 100)
+    #print(random_seed_list)
+
+    for seed in [6874,6874]:#random_seed_list:
+        print("SEED ", seed) # 6874
+        game = agent.play(seed)
         agent.calculate_state_values(game)
         print(agent.board_states)
+        
 
 
--- Exercise3/game.py
+++ Exercise3/game.py
@@ -13,7 +13,7 @@ class TicTacToe():
 
 
 
-    def move(self, player):
+    def move(self, player,seed):
 
         # Get empty cells not being played yet
         empty_cells = np.argwhere(self.board == 0)
@@ -21,13 +21,24 @@ class TicTacToe():
         # First, check if any cell is still empty 
         if len(empty_cells) == 0:
             # It's a draw
-            print("It's a draw")
+            print("It's a draw MOVE")
             self.EndFlag = True
+            self.history.append(','.join(str(int(num)) for row in self.board for num in row))
+        # elif len(empty_cells) == 1:
+        #     self.history.append(','.join(str(int(num)) for row in self.board for num in row))
+
+        #     # Pick random index
+        #     random.seed(seed)
+        #     random_index = random.choices(empty_cells)
+        #     self.board[random_index[0][0]][random_index[0][1]] = player
+
+        #     if self.checkState(player) ==
+
         else:
             self.history.append(','.join(str(int(num)) for row in self.board for num in row))
 
             # Pick random index
-            random.seed(42)
+            random.seed(seed)
             random_index = random.choices(empty_cells)
             self.board[random_index[0][0]][random_index[0][1]] = player
 
@@ -45,6 +56,13 @@ class TicTacToe():
         empty_cells = np.argwhere(self.board == 0)
         if len(empty_cells) <= 5: 
            self.winner = self.checkState(player)
+
+        if len(empty_cells) == 0:
+            print("It's a draw PLACEMENT")
+            self.EndFlag = True
+
+        print("PLACEMENT ", self.board)
+        
         
         
 
@@ -73,24 +91,25 @@ class TicTacToe():
 
         
 
-    def play(self):
+    # def play(self):
 
-        for i in range(9):
+    #     for i in range(9):
             
-            if self.EndFlag:
-                self.history.append(','.join(str(int(num)) for row in self.board for num in row))
-                break
-            else:   
-                if i % 2 == 0:
-                    self.move(self.player0)
-                else:
-                    self.move(self.player1)
-        print(self.board)
+    #         if self.EndFlag:
+    #             print("End Flag ", self.board)
+    #             self.history.append(','.join(str(int(num)) for row in self.board for num in row))
+    #             break
+    #         else:   
+    #             if i % 2 == 0:
+    #                 self.move(self.player0)
+    #             else:
+    #                 self.move(self.player1)
+    #     print(self.board)
             
-        return self.history, self.winner
+    #     return self.history, self.winner
 
 
-if __name__ == "__main__":
-    game = TicTacToe()
+# if __name__ == "__main__":
+#     game = TicTacToe()
 
-    print(game.play())
+#     print(game.play())
